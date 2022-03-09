2592caabf6dbd3903770c823cbba12caf8ee0107
Delta <dhriti.baruah20@gmail.com>
2022-03-09 23:16:09 +0530

    ball.js completed

--- Ping Pong/Ball.js
+++ Ping Pong/Ball.js
@@ -1,3 +1,6 @@
+const INITIAL_VELOCITY = 0.025
+const VELOCITY_INCREASE = 0.00001
+
 export default class Ball{
     constructor(ballElem){
         this.ballElem = ballElem
@@ -22,14 +25,39 @@ export default class Ball{
         this.ballElem.style.setProperty("--y", value)
     }
 
+    rect(){
+        return this.ballElem.getBoundingClientRect()
+    }
     reset(){
         this.x = 50
         this.y = 50
-        this.direction = {x:0.75, y:0.5}
+        this.direction = {x:0} //default value of x direction
+        //To find the direction
+        while(
+            Math.abs(this.direction.x) <= 0.2 || Math.abs(this.direction.x) >= 0.9  //Checking that the ball doesnot move in horizontal or vertical direction only
+        ){
+            const heading = randomNumberBetween(0, 2 * Math.PI)//in radian
+            this.direction =  { x: Math.cos(heading), y: Math.sin(heading) }
+        }
+        this.velocity = INITIAL_VELOCITY
     }
 
     update(delta){
-        this.x = 5
-        this.y = 15
+        this.x += this.direction.x * this.velocity*delta
+        this.y += this.direction.x * this.velocity*delta
+        this.velocity += VELOCITY_INCREASE * delta
+        const rect = this.rect()
+
+        if(rect.bottom >= window.innerHeight || rect.top <= 0){
+            this.direction.y *= -1
+        }
+
+        if(rect.right >= window.innerWidth || rect.left <= 0){
+            this.direction.x *= -1
+        }
     }
+}
+
+function randomNumberBetween(min, max){
+    return Math.random() * (max - min) + min //takes in a random number multiply it by the number within our range and add to it the minimum
 }
